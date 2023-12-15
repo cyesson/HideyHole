bdd7f18892543f6d902cc102e5394fde8e4697be
cyesson <chris.yesson@ioz.ac.uk>
2023-12-15 18:34:21 +0000

    document test data

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' @title Example terrain grid
+#'
+#' @description Example digital elevation model (made up data)
+#'
+#' @format A terra::rast grid dimensions 100 x 100 pixels
+#' @source <https://github.com/cyesson/HideyHole/tree/main/data>
+"TestGrid"
